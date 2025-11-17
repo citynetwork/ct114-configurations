c88d1d46723d250db11a804cbc36fe7ba18e60d6
Namrata Sitlani <namrata.sitlani@cleura.com>
2025-11-17 17:54:09 +0530

    Update templates to enable config_drive for server instances
    Update template to use config_drive instead of metadata
    service for instance configuration

--- 3_instance_fullnet/deploy.tf
+++ 3_instance_fullnet/deploy.tf
@@ -70,6 +70,7 @@ resource "openstack_blockstorage_volume_v3" "boot_volume" {
 resource "openstack_compute_instance_v2" "instance" {
   name = var.instance_name
   flavor_name = var.instance_flavor
+  config_drive = true
 
   block_device {
     uuid                  = openstack_blockstorage_volume_v3.boot_volume.id
